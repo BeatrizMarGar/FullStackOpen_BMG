303699b5ab1f092dae5d391da2a60c8c9b0d76e9
Bea <martin_beatriz94@hotmail.com>
2024-04-16 12:00:58 +0200

    part1_react_first_props

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,16 +1,21 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
-  console.log(now, a+b)
 
-
+  const name = 'Peter'
+  const age = '10'
 return (
   <div>
-    <p>Hello world, it is {now.toString()}</p>
-    <p>
-      {a} plus {b} is {a + b}
-    </p>
+   <h1>Greetings</h1>
+   <Hello name="Bea" age={20 + 10}/>
+   <Hello name={name} age={age} />
   </div>
 )
 }
